874ba1d869103411f31d95c08db33d7725bf0a6c
tuyenxngoc <tuyenn9448@gmail.com>
2024-11-07 00:15:59 +0700

    Initial commit

---
+++ src/main/java/com/teamobi/avatar2/Avatar2Server.java
@@ -0,0 +1,11 @@
+package com.teamobi.avatar2;
+
+/**
+ * @author tuyen
+ */
+public class Avatar2Server {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
